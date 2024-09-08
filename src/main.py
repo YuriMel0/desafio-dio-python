9d22955dad59b84a409ad5d12811d86cef2e7b2c
YuriMel0 <meloy316@gmail.com>
2024-09-08 19:58:16 -0300

    adicionando modificações no template

--- src/main.py
+++ src/main.py
@@ -8,7 +8,6 @@ menu = """
 ==>"""
 
 saldo = 0
-limite = 500
 extrato = ""
 numero_saques = 0
 LIMITES_SAQUES = 3
@@ -18,16 +17,22 @@ while True:
 
     if opcao == "d":
         valor_deposito = float(input("Informe o valor: "))
-        saldo = valor_deposito if valor_deposito > 0 else print("[ERRO] Valor invalido!")
+        if valor_deposito > 0:
+            saldo = valor_deposito
+            extrato += f"Deposito R$: {valor_deposito:.2f}\n"
+        else:
+            print("[ERRO] Valor invalido!")
     elif opcao == "s":
         valor_saque = float(input("informe o valor:"))
         if LIMITES_SAQUES > 0 and valor_saque <= 500:
             saldo -= valor_saque
             LIMITES_SAQUES -= 1
+            extrato += f"Valor saque R$: {valor_saque:.2f}\n"
         else:
             print("[ERRO] O valor excede o limmite por saque!") if valor_saque > 500 else print("[ERRO] Quantidade de saque excedido")
     elif opcao == "e":
-        print(F" Saldo atual : {saldo}")
+        print("EXTRATO")
+        print(extrato)
     elif opcao == "q":
         break
     else:

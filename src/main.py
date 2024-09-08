60f916dd626243df59ff64d8070f861874a5a750
YuriMel0 <meloy316@gmail.com>
2024-09-08 19:44:49 -0300

    adicionando o desafio_V1, usando implementação procedural

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 menu = """
-
+    OPERAÇÔES
 [d] Depositar
 [s] Sacar
 [e] Extrato
@@ -17,11 +17,17 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        print("desenvolver deposito")
+        valor_deposito = float(input("Informe o valor: "))
+        saldo = valor_deposito if valor_deposito > 0 else print("[ERRO] Valor invalido!")
     elif opcao == "s":
-        print("desenvolver saque")
+        valor_saque = float(input("informe o valor:"))
+        if LIMITES_SAQUES > 0 and valor_saque <= 500:
+            saldo -= valor_saque
+            LIMITES_SAQUES -= 1
+        else:
+            print("[ERRO] O valor excede o limmite por saque!") if valor_saque > 500 else print("[ERRO] Quantidade de saque excedido")
     elif opcao == "e":
-        print("desenvolver Extrato")
+        print(F" Saldo atual : {saldo}")
     elif opcao == "q":
         break
     else:
